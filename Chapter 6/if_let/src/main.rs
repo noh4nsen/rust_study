baa61f06a460cde3544d2a69947c1e609440bfa1
noh4nsen <noh4nsen@gmail.com>
2023-03-02 23:24:36 -0300

    chapter 6.3

--- Chapter 6/if_let/src/main.rs
+++ Chapter 6/if_let/src/main.rs
@@ -2,5 +2,8 @@ fn main() {
     let config_max = Some(9u8);
     if let Some(max) = config_max {
         println!("The maximum is configured to be {:?}", max);
+    } else {
+        println!("No maximum is configured");
     }
+
 }
