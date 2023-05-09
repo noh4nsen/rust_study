4f23442098b1e06e7f3ce9568271be848e28f6e8
noh4nsen <noh4nsen@gmail.com>
2023-05-09 02:04:47 -0300

    minigrep implementation

--- Chapter 12/minigrep/src/main.rs
+++ Chapter 12/minigrep/src/main.rs
@@ -1,28 +1,18 @@
+use chrono::prelude::*;
+use minigrep::Config;
 use std::env;
-use std::fs;
-
-struct Config {
-    query: String,
-    file_path: String,
-}
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    
-    let config = parse_config(&args);
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("[{}] Problem parsing arguments: {err}", Local::now());
+        process::exit(1);
+    });
 
-    let contents = fs::read_to_string(config.file_path)
-                                .expect("Should have been able to read the file");
-
-    println!("With text:\n{contents}");
-}
-
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
-
-    Config { query, file_path }
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("[{}] Application error: {e}", Local::now());
+        process::exit(1);
+    };
 }
